14f2f98723f3e3cd4d2edf0dc4fd2075f0144b4b
Chandan Kumar <chandankumarchandankumar38851@gmail.com>
2026-09-17 10:21:53 +0000

    learn Variable, String, and String Functions

---
+++ first.py
@@ -0,0 +1,18 @@
+# a=10
+# b=20
+# print(a+b)
+
+# Strings
+
+name='chandan'
+age=25
+
+# ---------------------------String Functions Starts ---------------------------
+
+
+# print(type(name))
+# print(name.capitalize())
+# print(name.upper())
+# print(name.lower())
+# ---------------------------String Functions End ---------------------------
+# list
